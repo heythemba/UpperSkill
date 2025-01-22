6ff406ce58c37087aba4919147f4199fdf4851c9
Racem-BOUCHNAK <140710547+Racem-BOUCHNAK@users.noreply.github.com>
2025-01-22 19:42:34 +0000

    correcting naming conventions

--- backend/controllers/auth-ctrl.js
+++ backend/controllers/auth-ctrl.js
@@ -1,116 +1,122 @@
 import { generateTokenAndSetCookie } from "../tools/Token.js";
 import User from "../models/user-model.js";
 import bcrypt from "bcryptjs";
-//user signup, this function retrieves the necessary user data from the request body, validates the input, 
-//checks for existing usernames and emails, hashes the password, creates a new User document, and saves it to the database,
-//if successful, it generates a JWT token, sets it as a cookie in the response, and sends the user's information as a response.
+
+// User signup, this function retrieves the necessary user data from the request body, validates the input,
+// checks for existing usernames and emails, hashes the password, creates a new User document, and saves it to the database,
+// if successful, it generates a JWT token, sets it as a cookie in the response, and sends the user's information as a response.
 export const signup = async (req, res) => {
+  try {
+    const { fullName, username, email, password, isStudent } = req.body;
 
-	try {
-		const { fullName, username, email, password, is_student } = req.body;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
 
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		if (!emailRegex.test(email)) {
-			return res.status(400).json({ error: "Invalid email format" });
-		}
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
 
-		const existingUser = await User.findOne({ username });
-		if (existingUser) {
-			return res.status(400).json({ error: "Username is already taken" });
-		}
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
 
-		const existingEmail = await User.findOne({ email });
-		if (existingEmail) {
-			return res.status(400).json({ error: "Email is already taken" });
-		}
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 characters long" });
+    }
 
-		if (password.length < 6) {
-			return res.status(400).json({ error: "Password must be at least 6 characters long" });
-		}
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+      isStudent,
+    });
 
-		const newUser = new User({
-			fullName,
-			username,
-			email,
-			password: hashedPassword,
-			is_student,
-		});
+    if (newUser) {
+      console.dir(newUser, { depth: null });
+      generateTokenAndSetCookie(newUser._id, res);
 
-		if (newUser) {
-			console.dir(newUser,{depth: null})
-			generateTokenAndSetCookie(newUser._id, res);
-			
-			await newUser.save();
-			
-			res.status(201).json({
-				_id: newUser._id,
-				fullName: newUser.fullName,
-				username: newUser.username,
-				email: newUser.email,
-				is_student: newUser.is_student,
-				profileImg: newUser.profileImg,
-			});
-		} else {
-			res.status(400).json({ error: "Invalid user data" });
-		}
-	} catch (error) {
-		console.log("Error in signup controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        isStudent: newUser.isStudent,
+        profileImg: newUser.profileImg,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-//user login, this function retrieves the email and password from the request body, 
-//finds the user in the database, compares the hashed password, and generates a JWT token if the credentials are correct,
-//it then sets the JWT token as a cookie in the response and sends the user's information as a response.
+
+// User login, this function retrieves the email and password from the request body,
+// finds the user in the database, compares the hashed password, and generates a JWT token if the credentials are correct,
+// it then sets the JWT token as a cookie in the response and sends the user's information as a response.
 export const login = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const user = await User.findOne({ email });
-		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
 
-		if (!user || !isPasswordCorrect) {
-			return res.status(400).json({ error: "Invalid email or password" });
-		}
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
 
-		generateTokenAndSetCookie(user._id, res);
+    generateTokenAndSetCookie(user._id, res);
 
-		res.status(200).json({
-			_id: user._id,
-			fullName: user.fullName,
-			username: user.username,
-			email: user.email,
-			is_student: user.is_student,
-			profileImg: user.profileImg,
-			coverImg: user.coverImg,
-		});
-	} catch (error) {
-		console.log("Error in login controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      isStudent: user.isStudent,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-//user logout, this functionhandles the user logout process,
-//it clears the JWT token from the cookie in the response and sends a success message.
+
+// User logout, this function handles the user logout process,
+// it clears the JWT token from the cookie in the response and sends a success message.
 export const logout = async (req, res) => {
-	try {
-		res.cookie("jwt", "", { maxAge: 0 });
-		res.status(200).json({ message: "Logged out successfully" });
-	} catch (error) {
-		console.log("Error in logout controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-//this function retrieves the current user's information from the database based on the user ID stored in the request,
-//it selects the password field to exclude it from the response and sends the user's information as a response.
+// This function retrieves the current user's information from the database based on the user ID stored in the request,
+// it selects the password field to exclude it from the response and sends the user's information as a response.
 export const getMe = async (req, res) => {
-	try {
-		const user = await User.findById(req.user._id).select("-password");
-		res.status(200).json(user);
-	} catch (error) {
-		console.log("Error in getMe controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getMe controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-
--- backend/controllers/course-ctrl.js
+++ backend/controllers/course-ctrl.js
@@ -2,138 +2,147 @@ import Course from "../models/course-model.js";
 import User from "../models/user-model.js";
 import { v2 as cloudinary } from "cloudinary";
 
-//retrieves the necessary data from the request body, validates the input,
-//uploads the course image to Cloudinary, creates a new Course document,
-//and saves it to the database.
+// Retrieves the necessary data from the request body, validates the input,
+// uploads the course image to Cloudinary, creates a new Course document,
+// and saves it to the database.
 export const createCourse = async (req, res) => {
-	try {
-		const { title, discription, content } = req.body;
-		let { img } = req.body;
-		const userId = req.user._id.toString();
+  try {
+    const { title, description, content } = req.body;
+    let { img } = req.body;
+    const userId = req.user._id.toString();
 
-		const user = await User.findById(userId);
-		if (!user) return res.status(404).json({ message: "User not found" });
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-		if (!title || !discription || !content || !img) {
-			return res.status(400).json({ error: "fill all the field" });
-		}
+    if (!title || !description || !content || !img) {
+      return res.status(400).json({ error: "fill all the field" });
+    }
 
-		if (img) {
-			const uploadedResponse = await cloudinary.uploade>r.upload(img,{});
-			img = uploadedResponse.secure_url;
-		}
+    if (img) {
+      const uploadedResponse = await cloudinary.uploader.upload(img, {});
+      img = uploadedResponse.secure_url;
+    }
 
+    const newCourse = new Course({
+      user: userId,
+      title,
+      description,
+      content,
+      img,
+    });
 
-		const newCourse = new Course({
-			user: userId,
-			title,
-			discription,
-			content,
-			img,
-		});
-
-		await newCourse.save();
-		res.status(201).json(newCourse);
-	} catch (error) {
-		res.status(500).json({ error: "Internal server error" });
-		console.log("Error in createCourse ", error);
-	}
+    await newCourse.save();
+    res.status(201).json(newCourse);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in createCourse ", error);
+  }
 };
-//finds the course by its ID, checks if the user is authorized to delete the course,
-//deletes the course image from Cloudinary, and removes the course from the database.
+
+// Finds the course by its ID, checks if the user is authorized to delete the course,
+// deletes the course image from Cloudinary, and removes the course from the database.
 export const deleteCourse = async (req, res) => {
-	try {
-		const course = await Course.findById(req.params.id);
-		if (!course) {
-			return res.status(404).json({ error: "Course not found" });
-		}
+  try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).json({ error: "Course not found" });
+    }
 
-		if (course.user.toString() !== req.user._id.toString()) {
-			return res.status(401).json({ error: "You are not authorized to delete this Course" });
-		}
+    if (course.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete this Course" });
+    }
 
-		if (course.img) {
-			const imgId = course.img.split("/").pop().split(".")[0];
-			await cloudinary.uploader.destroy(imgId);
-		}
+    if (course.img) {
+      const imgId = course.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
 
-		await Course.findByIdAndDelete(req.params.id);
+    await Course.findByIdAndDelete(req.params.id);
 
-		res.status(200).json({ message: "Course deleted successfully" });
-	} catch (error) {
-		console.log("Error in deleteCourse ", error);
-		res.status(500).json({ error: "Internal server error" });
-	}
+    res.status(200).json({ message: "Course deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteCourse ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-//retrieves all courses from the database and sorts them by title in ascending order,
-//then sends the list of courses as a response to the client.
-export const getAllcourses = async (req, res) => {
-	try {
-		const course = await Course.find()
-			.sort({ title: 1 })
 
-		if (course.length === 0) {
-			return res.status(200).json([]);
-		}
+// Retrieves all courses from the database and sorts them by title in ascending order,
+// then sends the list of courses as a response to the client.
+export const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find().sort({ title: 1 });
 
-		res.status(200).json(course);
-	} catch (error) {
-		console.log("Error in getallcourses : ", error);
-		res.status(500).json({ error: "Internal server error" });
-	}
+    if (courses.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(courses);
+  } catch (error) {
+    console.log("Error in getallcourses : ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-//enroll a user to a specific course, it retrieves the user ID and course ID from the request parameters, 
-//checks if the user is already enrolled in the course, adds the user to the course's enrolled list, 
-//and updates the user's enrolled courses list.
+
+// Enroll a user to a specific course, it retrieves the user ID and course ID from the request parameters,
+// checks if the user is already enrolled in the course, adds the user to the course's enrolled list,
+// and updates the user's enrolled courses list.
 export const enrollToCourse = async (req, res) => {
-	try {
-		const userId = req.user._id;
-		const { id: courseId } = req.params;
-		
+  try {
+    const userId = req.user._id;
+    const { id: courseId } = req.params;
 
-		const course = await Course.findById(courseId);
-		const user = req.user;
+    const course = await Course.findById(courseId);
+    const user = req.user;
 
-		if (!courseId) {
-			return res.status(404).json({ error: "course not found" });
-		}
-		const enrolled = course.enroll.includes(userId);
+    if (!courseId) {
+      return res.status(404).json({ error: "Course not found" });
+    }
+    const enrolled = course.enroll.includes(userId);
 
-		if (!enrolled) {
+    if (!enrolled) {
+      course.enroll.push(userId);
+      await User.updateOne(
+        { _id: userId },
+        { $push: { courseenroll: courseId } }
+      );
+      await course.save();
 
-			course.enroll.push(userId);
-			await User.updateOne({ _id: userId }, { $push: { courseenroll: courseId } });
-			await course.save();
+      user.enrolled.push(courseId);
+      await Course.updateOne(
+        { _id: courseId },
+        { $push: { userenrolled: userId } }
+      );
+      await user.save();
 
-			user.enrolled.push(courseId);
-			await Course.updateOne({ _id: courseId }, { $push: { userenrolled: userId } });
-			await user.save();
-
-			res.status(200).json({ message: "Enrolled successfully" });
-
-		}else{
-			return res.status(400).json({ error: "You are already enrolled in this course" });
-        };
-	} catch (error) {
-		console.log("Error in enrollToCourse controller: ", error);
-		res.status(500).json({ error: "Internal server error" });
-	}
+      res.status(200).json({ message: "Enrolled successfully" });
+    } else {
+      return res
+        .status(400)
+        .json({ error: "You are already enrolled in this course" });
+    }
+  } catch (error) {
+    console.log("Error in enrollToCourse controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-//retrieves the list of courses that a specific user is enrolled in,
-//it finds the user by their ID, retrieves the enrolled courses from the database, 
-//and sends the list of enrolled courses as a response to the client.
+
+// Retrieves the list of courses that a specific user is enrolled in,
+// it finds the user by their ID, retrieves the enrolled courses from the database,
+// and sends the list of enrolled courses as a response to the client.
 export const yourCourses = async (req, res) => {
-	const userId = req.params.id;
+  const userId = req.params.id;
 
-	try {
-		const user = await User.findById(userId);
-		if (!user) return res.status(404).json({ error: "User not found" });
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
 
-		const enrolled = await Course.find({ _id: { $in: user.enrolled } })
+    const enrolled = await Course.find({ _id: { $in: user.enrolled } });
 
-		res.status(200).json(enrolled);
-	} catch (error) {
-		console.log("Error in yourCourses controller: ", error);
-		res.status(500).json({ error: "Internal server error" });
-	}
+    res.status(200).json(enrolled);
+  } catch (error) {
+    console.log("Error in yourCourses controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
